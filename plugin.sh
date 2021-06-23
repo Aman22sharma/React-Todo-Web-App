daeb50ee4dd7d7bc1b5bd2718e386e7f3c8c01a2
Aman22sharma <kumaraman.rose@gmail.com>
2021-06-23 10:16:30 +0530

    updated

---
+++ plugin.sh
@@ -0,0 +1,18 @@
+Commands used to install Font awesome:
+$ npm i --save @fortawesome/fontawesome-svg-core
+
+$ npm i --save @fortawesome/free-solid-svg-icons
+
+$ npm i --save @fortawesome/react-fontawesome
+
+Commands used to publish react files in github:
+
+git remote add origin https://github.com/Aman22sharma/React-Todo-Web-
+
+npm install gh-pages --save-dev // command to install github gh-pages
+
+git remote set-url origin https://github.com/Aman22sharma/React-Todo-Web-App
+
+npm run build
+
+npm run deploy
